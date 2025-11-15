7a95ba00290423defe11902aa169481a7dafed54
dankatadurev98 <dankatadurev@gmail.com>
2025-11-15 16:13:24 +0200

    server update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,24 @@
+import { Route, Routes } from "react-router"
 import Header from "./components/header/Header"
 import Footer from "./components/footer/Footer"
 import Body from './components/body/Body'
+import Catalog from "./components/catalog/Catalog"
 
 
 function App() {
-  
+
 
   return (
-  <>
- <Header/>
+    <>
+      <Header />
 
-  <Body/>
- 
- <Footer/>
-</>
+      <Routes>
+        <Route path="/" element={<Body />} />
+        <Route path="/catalog" element={<Catalog/>}/>
+      </Routes>
+      <Footer />
+
+    </>
 
   )
 }
--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,22 +1,26 @@
+import { Link } from "react-router"
+
+
 export default function Header(){
+
     return (
          <header>
     {/* Navigation */}
     <nav>
-      <a className="home" href="#">
+      <Link className="home" to="/">
         {" "}
         <img src="./images/logo.png" alt="logo" />{" "}
-      </a>
-      <a href="#">Catalog</a>
+      </Link>
+      <Link to="/catalog">Catalog</Link>
       {/* Logged-in users */}
       <div id="user">
-        <a href="#">Add Game</a>
-        <a href="#">Logout</a>
+        <Link to="/addGame">Add Game</Link>
+        <Link to="/logout">Logout</Link>
       </div>
       {/* Guest users */}
       <div id="guest">
-        <a href="#">Login</a>
-        <a href="#">Register</a>
+        <Link to="/login">Login</Link>
+        <Link to="/register">Register</Link>
       </div>
     </nav>
   </header>
